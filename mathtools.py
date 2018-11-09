84785cb808726cca681e20ffc429ce8c257c43bb
elisajuvonen <juvonen.elisa@gmail.com>
2018-11-09 14:05:24 +0200

    mathtools.py

--- mathtools.py
+++ mathtools.py
@@ -9,10 +9,18 @@ def isPrime(n):
             return False
     return True
 
-
 def factorial(n):
     '''Returns the factorial of a number'''
     if n == 0:
         return 1
     else:
         return n * factorial(n-1)
+
+def fib(n):
+    ''' Calculates the n value of the fibonacci sequence'''
+    if n == 0:
+        return 0
+    elif n == 1:
+        return 1
+    else:
+        return fib(n-1)+fib(n-2)
